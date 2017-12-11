aaf399efa5b0cb25aa609bd8294fa570c9b9c7d1
Rivera Soto, Rafael Alberto <riverasoto1@llnl.gov>
2017-12-10 20:34:05 -0800

    Added TL seq2seq_word_tl and twitter data from TL creator

---
+++ data/twitter/data.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+
+def load_data(PATH=''):
+    # read data control dictionaries
+    try:
+        with open(PATH + 'metadata.pkl', 'rb') as f:
+            metadata = pickle.load(f)
+    except:
+        metadata = None
+
+    idx_q = np.load(PATH + 'idx_q.npy')
+    idx_a = np.load(PATH + 'idx_a.npy')
+    return metadata, idx_q, idx_a
+
+def split_dataset(x, y, ratio = [0.7, 0.15, 0.15] ):
+    # number of examples
+    data_len = len(x)
+    lens = [ int(data_len*item) for item in ratio ]
+
+    trainX, trainY = x[:lens[0]], y[:lens[0]]
+    testX, testY = x[lens[0]:lens[0]+lens[1]], y[lens[0]:lens[0]+lens[1]]
+    validX, validY = x[-lens[-1]:], y[-lens[-1]:]
+
+    return (trainX,trainY), (testX,testY), (validX,validY)
