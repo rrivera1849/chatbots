a53c77ec1b6c301b1a66a70d170b29f3373bcbd3
Rivera Soto, Rafael Alberto <riverasoto1@llnl.gov>
2017-12-11 20:23:58 -0800

    Added script to create retrieval dataset and the dataset itself

---
+++ data/twitter/create_retrieval.py
@@ -0,0 +1,50 @@
+
+import os
+import pdb
+import sys
+from random import shuffle
+
+import numpy as np
+import pandas as pd
+
+def load_data():
+    q = np.load('idx_q.npy')
+    a = np.load('idx_a.npy')
+
+    data_len = len(q)
+    train_len = int(0.7 * data_len)
+    trainX, trainY = q[:train_len], a[:train_len]
+
+    return trainX.tolist(), trainY.tolist()
+
+def to_string(l):
+    result = []
+    for elem in l:
+        elem = [str(x) for x in elem]
+        result.append(' '.join(elem))
+    return result
+
+def main():
+    trainX, trainY = load_data()
+    trainX = to_string(trainX)
+    trainY = to_string(trainY)
+
+    data_correct = pd.DataFrame()
+    data_correct['context'] = trainX
+    data_correct['utterance'] = trainY
+    data_correct['label'] = 1
+
+    shuffle(trainY)
+
+    data_incorrect = pd.DataFrame()
+    data_incorrect['context'] = trainX
+    data_incorrect['utterance'] = trainY
+    data_incorrect['label'] = 0
+
+    result = pd.concat([data_correct, data_incorrect]).reset_index(drop=True)
+    result.to_csv('twitter_retrieval.csv')
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
