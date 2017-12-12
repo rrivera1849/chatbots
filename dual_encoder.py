c57b540ed100e25c1cd52fda60a376af15995722
Rivera Soto, Rafael Alberto <riverasoto1@llnl.gov>
2017-12-11 23:03:40 -0800

    Dual encoder now ready to add run_twitter() function

--- dual_encoder.py
+++ dual_encoder.py
@@ -1,7 +1,9 @@
 
 import os
+import pdb
 import json
 import pickle
+import sys
 from optparse import OptionParser
 
 import keras
@@ -20,6 +22,7 @@ parser.add_option('--batch-size', type=int, default=128,
                   help='Number of batches to use during training and evaluation')
 parser.add_option('--validate-every', type=int, default=1000, 
                   help='Validate model every |num| iterations')
+parser.add_option('--twitter', action='store_true', default=False)
 
 parser.add_option('--embedding-dim', type=int, default=100,
                   help='Dimensionality of the word embedding vectors')
@@ -30,59 +33,62 @@ parser.add_option('--max-context-length', type=int, default=160,
 parser.add_option('--max-utterance-length', type=int, default=80,
                   help='Maximum length for each utterance/distractor')
 
-(options, args) = parser.parse_args()
+all_results = {}
 
-dataset_path = 'datasets/preprocessed'
-train_path = os.path.join(dataset_path, 'train_preprocessed.json')
-validation_path = os.path.join(dataset_path, 'validation_preprocessed.json')
-vocabulary_path = os.path.join(dataset_path, 'vocabulary.json')
+def load_udc():
+    dataset_path = 'datasets/preprocessed'
+    train_path = os.path.join(dataset_path, 'train_preprocessed.json')
+    validation_path = os.path.join(dataset_path, 'validation_preprocessed.json')
+    vocabulary_path = os.path.join(dataset_path, 'vocabulary.json')
 
-train = np.array(json.load(open(train_path, 'r')))
-validation = np.array(json.load(open(validation_path, 'r')))
-vocab = json.load(open(vocabulary_path, 'r'))
+    train = np.array(json.load(open(train_path, 'r')))
+    validation = np.array(json.load(open(validation_path, 'r')))
+    vocab = json.load(open(vocabulary_path, 'r'))
 
-contexts, utterances, labels = train[:,0], train[:,1], train[:,2]
-val_contexts, val_utterances, val_distractors = validation[:,0], validation[:,1], validation[:,2:]
+    contexts, utterances, labels = train[:,0], train[:,1], train[:,2]
+    val_contexts, val_utterances, val_distractors = validation[:,0], validation[:,1], validation[:,2:]
 
-# Pad all of our train / validation sequences
-contexts = pad_sequences(contexts, maxlen=options.max_context_length, value=len(vocab)) 
-utterances = pad_sequences(utterances, maxlen=options.max_utterance_length, value=len(vocab))
+    # Pad all of our train / validation sequences
+    contexts = pad_sequences(contexts, maxlen=options.max_context_length, value=len(vocab)) 
+    utterances = pad_sequences(utterances, maxlen=options.max_utterance_length, value=len(vocab))
 
-val_contexts = pad_sequences(val_contexts, maxlen=options.max_context_length, value=len(vocab)) 
-val_utterances = pad_sequences(val_utterances, maxlen=options.max_utterance_length, value=len(vocab))
+    val_contexts = pad_sequences(val_contexts, maxlen=options.max_context_length, value=len(vocab)) 
+    val_utterances = pad_sequences(val_utterances, maxlen=options.max_utterance_length, value=len(vocab))
 
-distractors = []
-for i in range(val_distractors.shape[1]):
-    padded_distractor = pad_sequences(val_distractors[:, i], maxlen=options.max_utterance_length, value=len(vocab)) 
-    distractors.append(padded_distractor)
+    distractors = []
+    for i in range(val_distractors.shape[1]):
+        padded_distractor = pad_sequences(val_distractors[:, i], maxlen=options.max_utterance_length, value=len(vocab)) 
+        distractors.append(padded_distractor)
 
-##################################################
-# Build Model START
-##################################################
-# Placeholders for context and utterance inputs
-context_sequence = Input((options.max_context_length,))
-utterance_sequence = Input((options.max_utterance_length,))
+    return contexts, utterances, val_contexts, val_utterances, distractors, vocab
 
-# Add Embedding Layer and LSTM network to encode our inputs
-encoder = Sequential()
-encoder.add(Embedding(len(vocab)+1, output_dim=options.embedding_dim))
-encoder.add(LSTM(options.rnn_dim))
+def build_dual_encoder(max_context_length, max_utterance_length, vocab_size):
+    # Placeholders for context and utterance inputs
+    context_sequence = Input((options.max_context_length,))
+    utterance_sequence = Input((options.max_utterance_length,))
 
-context_encoded = encoder(context_sequence)
-utterance_encoded = encoder(utterance_sequence)
+    # Add Embedding Layer and LSTM network to encode our inputs
+    encoder = Sequential()
+    encoder.add(Embedding(len(vocab)+1, output_dim=options.embedding_dim))
+    encoder.add(LSTM(options.rnn_dim))
 
-# M is a rnn_dim x rnn_dim matrix used to get the predicted utterance
-M = Dense(options.rnn_dim, input_shape=(None, options.rnn_dim), use_bias=False)
+    context_encoded = encoder(context_sequence)
+    utterance_encoded = encoder(utterance_sequence)
 
-# Get similarity and score for our inputs
-predicted_utterance = M(context_encoded)
-similarity = dot([predicted_utterance, utterance_encoded], axes=(1,1))
-score = Activation('sigmoid')(similarity)
+    # M is a rnn_dim x rnn_dim matrix used to get the predicted utterance
+    M = Dense(options.rnn_dim, input_shape=(None, options.rnn_dim), use_bias=False)
 
-model = Model([context_sequence, utterance_sequence], score)
-##################################################
-# Build Model END 
-##################################################
+    # Get similarity and score for our inputs
+    predicted_utterance = M(context_encoded)
+    similarity = dot([predicted_utterance, utterance_encoded], axes=(1,1))
+    score = Activation('sigmoid')(similarity)
+
+    model = Model([context_sequence, utterance_sequence], score)
+
+    # Compile the model in order to be ready for training
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    return model
 
 def recall_at_k(predictions, target=0, k=[1,2,5,10]):
     results = {}
@@ -97,8 +103,7 @@ def recall_at_k(predictions, target=0, k=[1,2,5,10]):
 
     return results
 
-all_results = {}
-def evaluate_on_validation(epoch, logs, model):
+def udc_evaluate_on_validation(epoch, logs, model):
     print('[{}/{}] Evaluating on validation:'.format(epoch, options.num_epochs))
 
     predictions = model.predict([val_contexts, val_utterances], batch_size=options.batch_size)
@@ -111,26 +116,42 @@ def evaluate_on_validation(epoch, logs, model):
     for k, v in results.items():
         print('\t{}: {}'.format(k,v))
 
-# Compile the model in order to be ready for training
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-# Create callback to save a model checkpoint every epoch
-filepath = 'dual_encoder_best.hdf5'
-checkpointer = ModelCheckpoint(filepath, save_best_only=True, monitor='loss', mode='min')
-
-# Create a callback to evaluate on our validation data every epoch
-evaluate_model_cb = keras.callbacks.LambdaCallback(on_epoch_begin=lambda epoch, logs: evaluate_on_validation(epoch, logs, model))
-
-# Add callback to save Loss History
 class LossHistory(keras.callbacks.Callback):
     def on_train_begin(self, logs={}):
         self.losses = []
         
     def on_batch_end(self, batch, logs={}):
         self.losses.append(logs.get('loss'))
-loss_history = LossHistory()
 
-# Train the model
-model.fit([contexts, utterances], labels, batch_size=options.batch_size, epochs=options.num_epochs, callbacks=[checkpointer, evaluate_model_cb, loss_history])
-pickle.dump(loss_history.losses, open('dual_encoder_loss_history.pkl', 'wb'))
-pickle.dump(all_results, open('dual_encoder_all_results.pkl', 'wb'))
+def run_udc(options, args):
+    # Create callback to save a model checkpoint every epoch
+    pdb.set_trace()
+    contexts, utterances, val_contexts, val_utterances, distractors, vocab  = load_udc()
+    model = build_dual_encoder(options.max_context_length, max_utterance_length, len(vocab))
+
+    filepath = 'dual_encoder_best.hdf5'
+    checkpointer = ModelCheckpoint(filepath, save_best_only=True, monitor='loss', mode='min')
+
+    # Create a callback to evaluate on our validation data every epoch
+    evaluate_model_cb = keras.callbacks.LambdaCallback(on_epoch_begin=lambda epoch, logs: evaluate_on_validation(epoch, logs, model))
+    # Add callback to save Loss History
+    loss_history = LossHistory()
+
+    # Train the model
+    model.fit([contexts, utterances], labels, batch_size=options.batch_size, 
+              epochs=options.num_epochs, callbacks=[checkpointer, evaluate_model_cb, loss_history])
+    pickle.dump(loss_history.losses, open('dual_encoder_loss_history.pkl', 'wb'))
+    pickle.dump(all_results, open('dual_encoder_all_results.pkl', 'wb'))
+
+def main(options, args):
+    if options.twitter:
+        pass
+    else:
+        run_udc(options, args)
+
+    return 0
+
+if __name__ == '__main__':
+    (options, args) = parser.parse_args()
+
+    sys.exit(main(options, args))
